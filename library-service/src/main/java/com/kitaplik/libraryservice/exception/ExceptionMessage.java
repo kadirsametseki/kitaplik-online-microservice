13d83d579b643df57c5ca5c2b01f8e43bfe7a019
kadirsametseki <kadirsametseki1999.02@gmail.com>
2024-04-21 15:16:43 +0300

    exception handling

--- library-service/src/main/java/com/kitaplik/libraryservice/exception/ExceptionMessage.java
+++ library-service/src/main/java/com/kitaplik/libraryservice/exception/ExceptionMessage.java
@@ -1,9 +1,8 @@
 package com.kitaplik.libraryservice.exception;
 
-public record ExceptionMessage(
-        String timestamp,
-        int status,
-        String error,
-        String message,
-        String path) {
+public record ExceptionMessage(String timestamp,
+                               int status,
+                               String error,
+                               String message,
+                               String path) {
 }
