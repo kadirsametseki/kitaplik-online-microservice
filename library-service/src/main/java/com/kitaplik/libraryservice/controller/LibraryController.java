40fff6dc4c4484e2350f645d60c40d5f0e8a068f
kadirsametseki <kadirsametseki1999.02@gmail.com>
2024-05-30 01:06:31 +0300

    config

--- library-service/src/main/java/com/kitaplik/libraryservice/controller/LibraryController.java
+++ library-service/src/main/java/com/kitaplik/libraryservice/controller/LibraryController.java
@@ -6,6 +6,7 @@ import com.kitaplik.libraryservice.service.LibraryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RefreshScope
 @RequestMapping("/v1/library")
 public class LibraryController {
 
